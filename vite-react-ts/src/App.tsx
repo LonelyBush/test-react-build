6e39f6e5b2fd30de93f83d7dc544ef5bac457828
LonelyBush <lonely.bush68@gmail.com>
2024-05-03 14:31:39 +0300

    feat: add new p to App.tsx

--- vite-react-ts/src/App.tsx
+++ vite-react-ts/src/App.tsx
@@ -1,7 +1,12 @@
 import './style.scss';
 
 function App() {
-  return <div className="hello">Hello, React!</div>;
+  return (
+    <div className="hello">
+      Hello, React!
+      <p>Wow, child block!</p>
+    </div>
+  );
 }
 
 export default App;
